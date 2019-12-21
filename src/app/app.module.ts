55374645f40c1dbe201000306ed4b196e8447063
sorakhan <thesorakhan@gmail.com>
2019-12-22 00:46:57 +1100

    implemented firebase .. but not actual login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,36 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 const paths: {}[] = [
   {path: "" , component: HomeComponent},
   {path: "login" , component: LoginComponent}
 ]
 
+const firebaseConfig = {
+  apiKey: "AIzaSyApTf2zxKb3z03NPvFVLIyuwC0t_INjWsc",
+  authDomain: "blitz-e0d3d.firebaseapp.com",
+  databaseURL: "https://blitz-e0d3d.firebaseio.com",
+  projectId: "blitz-e0d3d",
+  storageBucket: "blitz-e0d3d.appspot.com",
+  messagingSenderId: "557222644142",
+  appId: "1:557222644142:web:10930c6f089e57f44b203f",
+  measurementId: "G-F3JRR1M4WM"
+};
+
 @NgModule({
   imports:      [ BrowserModule, 
                   FormsModule, 
                   MDBBootstrapModule.forRoot(),
-                  RouterModule.forRoot(paths) ],
+                  RouterModule.forRoot(paths),
+                  AngularFireModule.initializeApp(firebaseConfig),
+                  AngularFireDatabaseModule,
+                  AngularFirestoreModule,
+                  AngularFireAuthModule ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, LoginComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { email: string, password: string }
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  private itemDoc: AngularFirestoreDocument<Item>;
+  item: Observable<Item>;
 
-  ngOnInit() {
+  constructor(private afs: AngularFirestore) {
+    //Create a reference to a document on our database
+    this.itemDoc = afs.collection("users").doc("testUser");
+    //Creat a variable that holds the latest values of our database document
+    this.item = this.itemDoc.valueChanges();
+    //Extract the data from the database doc variable
+    this.item.subscribe(
+      user => {
+        this.email = user.email;
+        this.password = user.password
+      }
+    )
+   } // when the app starts, this loads
+
+  ngOnInit() { // any initialization that the app needs to complete
+
+  }
+
+  signIn() {
+    this.afs.collection("users").doc<Item>().set({email: this.email, password: this.password});
+    console.log(this.email + " , " + this.password);
   }
 
 }
